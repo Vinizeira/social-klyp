3683d1bc4b506396d8210cd3b52509a07ff79e2f
devlucas-java <devlucasjava@gmail.com>
2026-04-10 20:23:35 +0200

    feat: first commit

---
+++ src/main/java/com/github/devlucasjava/socialklyp/SocialKlypApplication.java
@@ -0,0 +1,13 @@
+package com.github.devlucasjava.socialklyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialKlypApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialKlypApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/devlucasjava/socialklyp/SocialKlypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devlucasjava.socialklyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialKlypApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
